2c15568a042b2191f69dfa35316aff53d123b53d
Abhishek Ravi 🌵 <ab.abhishek.ravi@gmail.com>
2024-04-24 08:32:44 +0530

    Initial Commit

---
+++ test-algorithm/MyAlgorithm.swift
@@ -0,0 +1,18 @@
+//
+//  MyAlgorithm.swift
+//  test-algorithm
+//
+//  Created by Abhishek Kumar Ravi on 24/04/24.
+//
+
+import Foundation
+
+public class MyAlgorithm {
+    
+    public init() {}
+    
+    public func prepareAlgo() {
+        print("This is my Algorithm 🌵")
+    }
+    
+}
---
+++ test-algorithm/test_algorithm.h
@@ -0,0 +1,18 @@
+//
+//  test_algorithm.h
+//  test-algorithm
+//
+//  Created by Abhishek Kumar Ravi on 24/04/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for test_algorithm.
+FOUNDATION_EXPORT double test_algorithmVersionNumber;
+
+//! Project version string for test_algorithm.
+FOUNDATION_EXPORT const unsigned char test_algorithmVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <test_algorithm/PublicHeader.h>
+
+
